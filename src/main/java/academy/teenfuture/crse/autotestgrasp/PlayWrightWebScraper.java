3cf1edf0f0ceab25d628137db94b0b920fc6bcee
Robin <robintsang@outlook.com>
2025-07-21 18:26:44 +0800

    Remove unrelated academy files from main source

--- src/main/java/academy/teenfuture/crse/autotestgrasp/PlayWrightWebScraper.java
+++
@@ -1,5 +0,0 @@
-package org.example.lib;
-
-public interface PlayWrightWebScraper<T,K> extends WebScraper<T,K>, PlayWrightLifeCycle {
-
-}
--- src/main/java/academy/teenfuture/crse/modal/ItemCode.java
+++
@@ -1,9 +0,0 @@
-package academy.teenfuture.crse.modal;
-
-import lombok.Data;
-
-@Data
-public class ItemCode {
-    String description;
-    int qty;
-}
